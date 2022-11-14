32b90dbcea72066fe411e9cca82c3f8d75e88e3a
unknown <Eneri93>
2022-11-14 19:46:26 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QApplication,QLabel,QWidget
+class Ventana(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("nueva ventana")
+        self.etiqueta1=QLabel("hola mundo",self)
+if __name__ == "__main__":
+    app=QApplication([])
+    ventana1=Ventana()
+    ventana1.show()
+    app.exec()
